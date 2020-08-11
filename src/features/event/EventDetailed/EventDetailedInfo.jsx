b8d9a54892ef2bcb90d119fbe2a585e9b1c0908a
krittiyaclark <krittiyaclark@gmail.com>
2020-08-10 21:14:01 -0400

    Compate adding the map to the Event Detailed Page

--- src/features/event/EventDetailed/EventDetailedInfo.jsx
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Segment, Button, Grid, Icon } from 'semantic-ui-react'
 import { format } from 'date-fns'
 import EventDetailedMap from './EventDetailedMap'
 
 function EventDetailedInfo({ event }) {
+	const [mapOpen, setMapOpenToggle] = useState(false)
+
 	return (
 		<Segment.Group>
 			<Segment attached='top'>
@@ -35,11 +37,16 @@ function EventDetailedInfo({ event }) {
 						<span>{event.venue.address}</span>
 					</Grid.Column>
 					<Grid.Column width={4}>
-						<Button color='teal' size='tiny' content='Show Map' />
+						<Button
+							onClick={() => setMapOpenToggle(!mapOpen)}
+							color='teal'
+							size='tiny'
+							content={mapOpen ? 'Hide map' : 'Show Map'}
+						/>
 					</Grid.Column>
 				</Grid>
 			</Segment>
-			<EventDetailedMap latLng={event.venue.latLng} />
+			{mapOpen && <EventDetailedMap latLng={event.venue.latLng} />}
 		</Segment.Group>
 	)
 }
