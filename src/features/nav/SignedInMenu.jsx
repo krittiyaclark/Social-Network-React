a6caee20f73cd80b291b2df0f1652a7e70982ddf
krittiyaclark <krittiyaclark@gmail.com>
2020-08-15 16:11:47 -0400

    Signing in & Signing out the user with Firebas

--- src/features/nav/SignedInMenu.jsx
+++ src/features/nav/SignedInMenu.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Menu, Image, Dropdown } from 'semantic-ui-react'
 import { Link, useHistory } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { signOutUser } from '../auth/authActions'
+import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
+import { signOutFirebase } from '../../app/firestore/firebaseService'
 
 function SignedInMenu() {
-	const dispatch = useDispatch()
 	const { currentUser } = useSelector((state) => state.auth)
 	const history = useHistory()
+
+	async function handleSignOut() {
+		try {
+			await signOutFirebase()
+			history.push('/')
+		} catch (error) {
+			toast.error(error.message)
+		}
+	}
 
 	return (
 		<Menu.Item position='right'>
@@ -25,14 +34,7 @@ function SignedInMenu() {
 						icon='plus'
 					/>
 					<Dropdown.Item text='My Profile' icon='user' />
-					<Dropdown.Item
-						onClick={() => {
-							dispatch(signOutUser())
-							history.push('/')
-						}}
-						text='Sign Out'
-						icon='power'
-					/>
+					<Dropdown.Item onClick={handleSignOut} text='Sign Out' icon='power' />
 				</Dropdown.Menu>
 			</Dropdown>
 		</Menu.Item>
