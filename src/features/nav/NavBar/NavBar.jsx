f81a0f856653cbe4231b51a0de7695aaea959b06
krittiyaclark <krittiyaclark@gmail.com>
2020-10-20 12:17:00 -0400

    Installed dependencies & remove Sandbox navigation

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -22,7 +22,6 @@ function NavBar() {
 						Social Network
 					</Menu.Item>
 					<Menu.Item as={NavLink} to='/events' name='Events' />
-					<Menu.Item as={NavLink} to='/sandbox' name='Sandbox' />
 					{authenticated && (
 						<Menu.Item as={NavLink} to='/createEvent'>
 							<Button positive inverted content='Create Event'></Button>
